4768cf65399e5a5d596229bc9ba8931f7397f35b
Sanjay-Kumar-SK-MCA-23 <sandysanjay1302@gmail.com>
2025-04-09 20:01:57 +0530

    backend update

--- Backend/index.js
+++ Backend/index.js
@@ -16,8 +16,8 @@ app.use(bodyParser.json());
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
 })
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.error(err));
